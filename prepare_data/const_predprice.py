19d1617d4c4151d85aeb88ebb28e12e2438c504c
Михаил Пузицкий <tarzan3668@gmail.com>
2024-02-29 18:51:13 +0300

    update

--- prepare_data/const_predprice.py
+++ prepare_data/const_predprice.py
@@ -3,7 +3,8 @@ BASE_COLS = ['Open', 'High', 'Low', 'Volume', 'Rsi', 'Tkr Buy', 'Tkr Sell',
        'Open Interest', 'Number Of Trades', 'Taker Buy Volume',
        'TT LongShortRatio Positions 5m', 'TT LongAccount Ratio Positions 5m',
        'TT ShortAccount Positions 5m', 'TT LongShortRatio Accounts 5m',
-       'TT LongAccount Accounts 5m', 'TT ShortAccount Accounts 5m']
+       'TT LongAccount Accounts 5m', 'TT ShortAccount Accounts 5m',
+       'Buy Sell Ratio 5m', 'Buy Vol 5m', 'Sell Vol 5m', ]
 
 # for model predict Close
 TARGET_COLS = ['Close']
