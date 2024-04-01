b9b3b5b9b2f2922aea972b684f7f001c0da79728
Михаил Пузицкий <gretawolf@live.ru>
2024-04-01 21:14:28 +0300

    scripts

---
+++ scripts/bots.py
@@ -0,0 +1,248 @@
+import numpy as np
+
+def cond_long (state,
+               id_pr_up,
+               id_tr_up,
+               id_pr_lw,
+               id_tr_lw,
+               correct_price,
+               id_pr_up_tresh,
+               id_tr_lw_tresh,
+               tresh_hold
+               ):
+
+    """
+    id_pr_up - id предсказаний выше текущей цены [0,1] из индексов [0,1,2,3,4]
+    id_tr_up - тренд рост id из индексов [0,1,2,3,4]
+    id_pr_lw - id предсказаний ниже текущей цены [0,1,3] из индексов [0,1,2,3,4]
+    id_tr_lw - тренд падения id из индексов [0,1,2,3,4]
+    correct_price - корректируем ли цену сравнивая прошлое и текцщее
+    id_pr_up_tresh - id предсказаний выше текущей с учетом tresh_hold
+    id_tr_lw_tresh - id предсказаний ниже текущей цены с учетом tresh_hold
+    tresh_hold - в % от cur_price
+    """
+    cur_price = state["last_price"]
+    delta = 0
+    if correct_price:
+        delta = state["pred_prices"][-1][0] - state["pred_prices"][-2][1]
+    # Вход лонг
+    # Покупаем когда
+    # контроль по цене если заданы индексы
+    if len(id_pr_up):
+        # Сетка 1 показывает id_pr_up предсказаний выше текущей цены И
+        cond_1 =  all(state["pred_prices"][-1][id_pr_up] + delta > cur_price)
+    # одобряем если нет контроля
+    else:
+        cond_1 = True
+
+    # контроль по цене c tresh_hold если заданы индексы
+    if len(id_pr_up_tresh):
+        # Сетка 1 показывает id_pr_up предсказаний выше текущей цены И
+        cond_1_t =  all(state["pred_prices"][-1][id_pr_up_tresh] + delta > cur_price*(100 + tresh_hold)/100)
+    # одобряем если нет контроля
+    else:
+        cond_1_t = True
+
+    # контроль по тренду если заданы индексы
+    if len(id_tr_up):
+        # Сетка 2 изменила состояние Флэт или Падение
+        cond_2 = all(state["pred_trend"][-2] < 1)
+        # на Рост по всем id_tr_up.
+        cond_3 = all(state["pred_trend"][-1][id_tr_up] == 1)
+        cond_4 = cond_2 and cond_3
+    # одобряем если нет контроля
+    else:
+        cond_4 = True
+    # Вход лонг
+    enter_long = cond_1 and cond_4 and cond_1_t
+
+
+    # Выход из лонга
+    # Продаем позицию если
+    # контроль по цене если заданы индексы
+    if len(id_pr_lw):
+        # Сетка 1 показывает id_pr_lw предсказания ниже текущей цены И
+        cond_5 =  all(state["pred_prices"][-1][id_pr_lw] + delta < cur_price)
+    # одобряем если нет контроля
+    else:
+        cond_5 = True
+
+    # контроль по цене c tresh_hold если заданы индексы
+    if len(id_tr_lw_tresh):
+        # Сетка 1 показывает id_pr_up предсказаний выше текущей цены И
+        cond_5_t =  all(state["pred_prices"][-1][id_tr_lw_tresh] + delta < cur_price*(100 - tresh_hold)/100)
+    # одобряем если нет контроля
+    else:
+        cond_5_t = True
+
+
+    # контроль по тренду если заданы индексы
+    if len(id_tr_lw):
+        # Сетка 2 изменила состояние на Падение на id_pr_lw минутах.
+        a = np.array(state["pred_trend"][-2][id_tr_lw])
+        cond_6 = all(a >= 0)
+        a = np.array(state["pred_trend"][-1][id_tr_lw])
+        cond_7 = all(a == -1)
+        cond_8 = cond_6 and cond_7
+    # одобряем если нет контроля
+    else:
+        cond_8 = True
+
+
+    # Выход из лонга
+    exit_long = cond_5 and cond_8 and cond_5_t
+
+    # Вход лонг
+    if enter_long and not exit_long : return 1  #
+    # Выход из лонга
+    elif exit_long and not enter_long : return -1  # 
+    # спим
+    else:  return 0
+
+
+
+def cond_short(state,
+               id_pr_up,
+               id_tr_up,
+               id_pr_lw,
+               id_tr_lw,
+               correct_price,
+               id_pr_up_tresh,
+               id_tr_lw_tresh,
+               tresh_hold
+                ):
+
+    """
+    id_pr_up - id предсказаний выше текущей цены [0,1] из индексов [0,1,2,3,4]
+    id_tr_up - тренд рост id из индексов [0,1,2,3,4]
+    id_pr_lw - id предсказаний ниже текущей цены [0,1,3] из индексов [0,1,2,3,4]
+    id_tr_lw - тренд падения id из индексов [0,1,2,3,4]
+    correct_price - корректируем ли цену сравнивая прошлое и текцщее
+    id_pr_up_tresh - id предсказаний выше текущей с учетом tresh_hold
+    id_tr_lw_tresh - id предсказаний ниже текущей цены с учетом tresh_hold
+    tresh_hold - в % от cur_price
+    """
+    cur_price = state["last_price"]
+    delta = 0
+    if correct_price:
+        delta = state["pred_prices"][-1][0] - state["pred_prices"][-2][1]
+    # Вход шорт
+    # Покупаем когда
+     # контроль по цене если заданы индексы
+    if len(id_pr_lw):
+        # Сетка 1 показывает id_pr_lw предсказаний ниже текущей цены И
+        cond_1 =  all(state["pred_prices"][-1][id_pr_lw] + delta < cur_price)
+    # одобряем если нет контроля
+    else:
+        cond_1 = True
+
+    # контроль по цене c tresh_hold если заданы индексы
+    if len(id_tr_lw_tresh):
+        # Сетка 1 показывает id_pr_lw предсказаний ниже текущей цены И
+        cond_1_t =  all(state["pred_prices"][-1][id_tr_lw_tresh] + delta < cur_price*(100 - tresh_hold)/100)
+    # одобряем если нет контроля
+    else:
+        cond_1_t = True
+
+    # контроль по тренду если заданы индексы
+    if len(id_tr_lw):
+        # Сетка 2 изменила состояние Флэт или Рост
+        cond_2 = all(state["pred_trend"][-2][id_tr_lw] > -1)
+        # Падение по всем пяти минутам.
+        cond_3 = all(state["pred_trend"][-1][id_tr_lw] == -1)
+        cond_4 = cond_2 and cond_3
+    # одобряем если нет контроля
+    else:
+        cond_4 = True
+
+    # Вход лонг
+    enter_short = cond_1 and cond_4 and cond_1_t
+
+    # Выход из шорта
+    # Продаем позицию если
+    # контроль по цене если заданы индексы
+    if len(id_pr_up):
+        # Сетка 1 показывает id_pr_up предсказания выше текущей цены  И
+        cond_5 =  all(state["pred_prices"][-1][id_pr_up] + delta > cur_price)
+    # одобряем если нет контроля
+    else:
+        cond_5 = True
+
+    # контроль по цене c tresh_hold если заданы индексы
+    if len(id_pr_up_tresh):
+        # Сетка 1 показывает id_pr_up_tresh предсказания выше текущей цены  И
+        cond_5_t =  all(state["pred_prices"][-1][id_pr_up_tresh] + delta > cur_price*(100 + tresh_hold)/100)
+    # одобряем если нет контроля
+    else:
+        cond_5_t = True
+
+
+    # контроль по цене если заданы индексы
+    if len(id_tr_up):
+        # Сетка 2 изменила состояние на Рост на  id_tr_up минуте
+        a = np.array(state["pred_trend"][-2][id_tr_up])
+        cond_6 = all(a <= 0)
+        a = np.array(state["pred_trend"][-1][id_tr_up])
+        cond_7 = all(a == 1)
+        con_8 = cond_6 and cond_7
+    # одобряем если нет контроля
+    else:
+        con_8 = True
+
+    # Выход из шорта
+    exit_short = cond_5 and con_8 and cond_5_t
+
+    # Вход шорта
+    if enter_short and not exit_short : return -1 # 
+    # Выход из шорта
+    elif exit_short and not enter_short: return 1 #  
+    # спим
+    else:  return 0
+    
+#######################################################################################
+
+def long_Short(action,
+               lg_id_pr_up = [],
+               lg_id_tr_up = [],
+               lg_id_pr_lw = [],
+               lg_id_tr_lw = [],
+               sh_id_pr_up = [],
+               sh_id_tr_up = [],
+               sh_id_pr_lw = [],
+               sh_id_tr_lw = [],
+               correct_price = False,
+               lg_id_pr_up_tresh = [],
+               lg_id_pr_lw_tresh = [],
+               sh_id_pr_up_tresh = [],
+               sh_id_pr_lw_tresh = [],
+               tresh_hold = 0.
+ ):
+    """
+    декоратор для управления функциями
+    action in ('long', 'short', 'mix')
+    """
+    def mix_action (state, long_action, short_action):
+      # получаем индексы шорта
+      short_act = short_action(state,
+                               sh_id_pr_up, sh_id_tr_up,
+                               sh_id_pr_lw, sh_id_tr_lw,
+                               correct_price,
+                               sh_id_pr_up_tresh, sh_id_pr_lw_tresh,
+                               tresh_hold
+                               )
+      # получаем индексы лонга
+      long_act = long_action(state,
+                             lg_id_pr_up, lg_id_tr_up,
+                             lg_id_pr_lw, lg_id_tr_lw,
+                             correct_price,
+                             lg_id_pr_up_tresh, lg_id_pr_lw_tresh,
+                             tresh_hold
+                             )
+
+      # используем long, short или оба
+      if action == 'long' : return [long_act, 0]
+      elif action == 'short' : return [0, short_act]
+      else:
+          return  [long_act, short_act]
+      
+    return mix_action
---
+++ scripts/robotest.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pandas as pd
+from scripts.bots import cond_long, cond_short
+
+from tqdm.notebook import tqdm   # отрисовка прохождения цикла
+
+def prepare_data(df_in: pd.DataFrame,
+                        depth:int,
+                        make_log,
+                        scalers,
+                        glass,
+                        features
+                      ):
+    # обогащаем данные ранее созданной функцией
+    ds = df_in.copy()
+    if make_log:
+       ds["Volume"] = ds["Volume"].apply(lambda x: np.log(x))
+    # Удаление строк с неполными данными
+    #ds = clean_dataset(ds)
+    # оставляем на глубину depth и только нужные колонки
+    x_data = ds[features][-depth:].values
+    # нормализуем
+    x_out = np.zeros_like(x_data)
+    x_out[:,:-glass] = scalers[0].transform(x_data[:,:-glass])  # трансформируем x_train
+    x_out[:,-glass:] = scalers[1].transform(x_data[:,-glass:])  # трансформируем x_train
+
+    # выводим с добавлением измерения по axis=0
+    return np.expand_dims(x_out, axis=0)
+
+def tresh_trend(trend, trsh_d, trsh_u):
+    out = np.zeros(trend.shape[0])
+    out[trend > trsh_u] = 1
+    out[trend < trsh_d] = -1
+    return out.astype(int)
+
+
+def making_signals(past_df: pd.DataFrame,
+                    check_df: pd.DataFrame,
+                    pred_lag: int,
+                    glass: int,
+                    #comis:float,
+                    depth: int,
+                    make_log_vol: bool,
+                    make_log_tgt: bool,
+                    fun_action: object,
+                    model_price: object,
+                    model_trend: object,
+                    scalers_price: object,
+                    scalers_trend: object,
+                    features_price: list,
+                    features_trend: list,
+                    #use_force_action = False,
+                    #show_unique_signals = True,
+                    ):
+
+    """
+    Args:
+        past_df (pd.DataFrame) - прошлые OHCL данные до тестируемых
+        check_df (pd.DataFrame) - тестируемые OHCL данные
+        pred_lag: int - шаг предсказания
+        model (class) - ранее обученная модель
+        use_force_action (bool): False - выводим чистые действия,
+                                 True - выводим действия с учетом уверенности,
+        show_unique_signals (bool) - показать ли состав чистых действий
+
+    Return:
+         df_signal(pd.DataFrame) - копия OHCL датафрейма из check_df с колонкой Signal
+    """
+    # сбор действий
+    all_actions = [[0, 0]]
+    # сбор последнего активного действия и цены
+    last_state = {
+        "last_price" : 0,
+        "pred_prices" : [[]],
+        "pred_trend" : [[]]
+                    }
+
+    # итеррируемся по длине изучаемого датафрейма
+    # берем на страте прошлые данные
+    for i in tqdm(range(check_df.shape[0]-pred_lag), unit ="step",
+                      desc ="Пробегаемся по всем отсчетам"):
+        if not i: # берем на страте прошлые данные
+            check_data = past_df.copy()
+        # далее отшипываем вначале 1 свечу
+        else: #  и присоединяем в коней новую из изучаемого датафрейма
+            check_data = check_data[1:].append(check_df[i:i+1])
+
+        price = check_data.Close[-1]
+        last_state["last_price"] = price
+
+        # обогащаем данные по аналогии как готовили для убучени
+        to_pred_price = prepare_data(df_in = check_data,
+                                      depth = depth, #DEPTH ,    # ранее заданная глубина сбора данных в прошлое
+                                      make_log = make_log_vol, #MAKE_LOG_VOL,
+                                      scalers = scalers_price,
+                                      glass = glass,
+                                      features = features_price
+                                      )
+
+        # предсказание модели price
+        pred_price = model_price.predict(to_pred_price, verbose=False)
+        pred_price = scalers_price[2].inverse_transform(pred_price)
+
+        if make_log_tgt : pred_price = np.exp(pred_price) #MAKE_LOG_TARGET
+        # собираем историю pred_prices и price
+        #last_state["pred_prices"] = pred_price[0].astype(float)
+        last_state["pred_prices"].append(pred_price[0].astype(float))
+
+        # обогащаем данные по аналогии как готовили для убучени
+        to_pred_trend = prepare_data(df_in = check_data,
+                                      depth = depth, #DEPTH ,    # ранее заданная глубина сбора данных в прошлое
+                                      make_log = make_log_vol, #MAKE_LOG_VOL,
+                                      scalers = scalers_trend,
+                                      glass = glass, #GLASS,
+                                      features = features_trend
+                                      )
+        # предсказание модели trend
+        pred_trend = model_trend.predict(to_pred_trend, verbose=False)[0].astype(float)
+        pred_trend = tresh_trend(pred_trend, trsh_d = -0.1, trsh_u = 0.1)
+        last_state["pred_trend"].append(pred_trend)
+
+        if i:
+          last_state["pred_prices"] = last_state["pred_prices"][-2:]
+          last_state["pred_trend"] = last_state["pred_trend"][-2:]
+          to_action = fun_action(last_state, cond_long, cond_short)
+          all_actions.append(to_action)
+          # раскомитить чтобы выводило to_action
+          if sum(to_action): print(to_action)
+
+
+
+    # переводим классы сигналов в массив сигналов
+    all_actions = np.vstack(all_actions)
+    # смещаем на pred_lag
+    df_signal = check_df[pred_lag:].copy()
+    df_signal['to_long']  = all_actions[:, 0]
+    df_signal['to_short'] = all_actions[:, 1]
+    # берем чистые действия действия
+    return  df_signal
